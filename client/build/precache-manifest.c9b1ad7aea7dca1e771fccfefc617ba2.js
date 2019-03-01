5dec175ea72c29e00ea16a5d00456dd83cd8eff3
wonder woman <rdelquiam@gmail.com>
2019-02-28 21:35:27 -0800

    build

---
+++ client/build/precache-manifest.c9b1ad7aea7dca1e771fccfefc617ba2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ff14be07cec140332245",
+    "url": "/static/js/main.ff14be07.chunk.js"
+  },
+  {
+    "revision": "b1b59baa7e7cda293337",
+    "url": "/static/js/2.b1b59baa.chunk.js"
+  },
+  {
+    "revision": "ff14be07cec140332245",
+    "url": "/static/css/main.b806e8ee.chunk.css"
+  },
+  {
+    "revision": "8b6f38f6e134625a181f1e4ed72a4392",
+    "url": "/index.html"
+  }
+];
